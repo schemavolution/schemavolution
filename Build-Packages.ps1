f4279057e12963ae2241e3fa1f7f0dcd109bc851
Michael L Perry <michael@qedcode.com>
2017-06-11 13:50:25 -0500

    Build all packages from one script.

--- Build-Packages.ps1
+++ Build-Packages.ps1
@@ -1,1 +1,14 @@
+cd .\Schemavolution.Specification\
+dotnet build
+dotnet pack --include-symbols
+cd ..\
+
+cd .\Schemavolution.DDD\
+dotnet build
+dotnet pack --include-symbols
+cd ..\
+
+cd .\Schemavolution.EF6\
 nuget pack -Build -Prop Configuration=Release
+nuget pack -Symbols
+cd ..\
