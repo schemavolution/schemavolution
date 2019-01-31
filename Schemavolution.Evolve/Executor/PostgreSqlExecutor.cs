85550c66c3b446d1f90c3d2d62dc661d33d6c117
Michael L Perry <michael@qedcode.com>
2019-01-31 17:41:12 -0600

    Describe the method not implemented.

--- Schemavolution.Evolve/Executor/PostgreSqlExecutor.cs
+++ Schemavolution.Evolve/Executor/PostgreSqlExecutor.cs
@@ -14,27 +14,27 @@ namespace Schemavolution.Evolve.Executor
 
         public bool DatabaseExists(string databaseName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("DatabaseExists");
         }
 
         public void DestroyDatabase(string databaseName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("DestroyDatabase");
         }
 
         public void ExecuteSqlCommands(IEnumerable<string> commands)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("ExecuteSqlCommands");
         }
 
         public List<EvolutionHistoryRow> LoadEvolutionHistory(string databaseName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("LoadEvolutionHistory");
         }
 
         public void UpgradeDatabase(string databaseName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("UpgradeDatabase");
         }
     }
 }
--- Schemavolution.Evolve/Providers/PostgreSqlProvider.cs
+++ Schemavolution.Evolve/Providers/PostgreSqlProvider.cs
@@ -7,87 +7,87 @@ namespace Schemavolution.Evolve.Providers
     {
         public string GenerateColumnDefinition(string columnName, string typeDescriptor, bool nullable)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateColumnDefinition");
         }
 
         public string[] GenerateCreateColumn(string databaseName, string schemaName, string tableName, string columnName, string typeDescriptor, bool nullable)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateCreateColumn");
         }
 
         public string[] GenerateCreateIndex(string databaseName, string schemaName, string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateCreateIndex");
         }
 
         public string[] GenerateCreatePrimaryKey(string databaseName, string schemaName, string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateCreatePrimaryKey");
         }
 
         public string GenerateCreateTable(string databaseName, string schemaName, string tableName, IEnumerable<string> definitions)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateCreateTable");
         }
 
         public string[] GenerateCreateUniqueIndex(string databaseName, string schemaName, string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateCreateUniqueIndex");
         }
 
         public string[] GenerateDropColumn(string databaseName, string schemaName, string tableName, string columnName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateDropColumn");
         }
 
         public string[] GenerateDropIndex(string databaseName, string schemaName, string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateDropIndex");
         }
 
         public string[] GenerateDropPrimaryKey(string databaseName, string schemaName, string tableName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateDropPrimaryKey");
         }
 
         public string GenerateDropTable(string databaseName, string schemaName, string tableName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateDropTable");
         }
 
         public string[] GenerateDropUniqueIndex(string databaseName, string schemaName, string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateDropUniqueIndex");
         }
 
         public string GenerateIndexDefinition(string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateIndexDefinition");
         }
 
         public string[] GenerateInitialization(string databaseName, string fileName)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateInitialization");
         }
 
         public string GenerateInsertStatement(string databaseName, IEnumerable<GeneMemento> genes)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateInsertStatement");
         }
 
         public string GeneratePrerequisiteInsertStatements(string databaseName, IEnumerable<GeneMemento> genes)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GeneratePrerequisiteInsertStatements");
         }
 
         public string GeneratePrimaryKeyDefinition(string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GeneratePrimaryKeyDefinition");
         }
 
         public string GenerateUniqueIndexDefinition(string tableName, IEnumerable<string> columnNames)
         {
-            throw new System.NotImplementedException();
+            throw new System.NotImplementedException("GenerateUniqueIndexDefinition");
         }
     }
 }
