759fca13854e44c03d8a1dc811500408b520b7fe
Michael L Perry <michael@qedcode.com>
2019-01-31 17:55:09 -0600

    Check to see if a database exists.

--- Schemavolution.Evolve/Executor/PostgreSqlExecutor.cs
+++ Schemavolution.Evolve/Executor/PostgreSqlExecutor.cs
@@ -1,5 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Schemavolution.Evolve.Loader;
+using Npgsql;
+using System.Linq;
 
 namespace Schemavolution.Evolve.Executor
 {
@@ -14,7 +17,10 @@ namespace Schemavolution.Evolve.Executor
 
         public bool DatabaseExists(string databaseName)
         {
-            throw new System.NotImplementedException("DatabaseExists");
+            var names = ExecuteSqlQuery($"SELECT datname from pg_database WHERE datname='{databaseName}';",
+                row => (string)row["datname"]);
+            bool exist = names.Any();
+            return exist;
         }
 
         public void DestroyDatabase(string databaseName)
@@ -35,6 +41,29 @@ namespace Schemavolution.Evolve.Executor
         public void UpgradeDatabase(string databaseName)
         {
             throw new System.NotImplementedException("UpgradeDatabase");
+        }
+
+        private List<T> ExecuteSqlQuery<T>(
+            string queryText,
+            Func<NpgsqlDataReader, T> read)
+        {
+            var result = new List<T>();
+            using (var connection = new NpgsqlConnection(_masterConnectionString))
+            {
+                connection.Open();
+                using (var command = connection.CreateCommand())
+                {
+                    command.CommandText = queryText;
+                    using (var reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            result.Add(read(reader));
+                        }
+                    }
+                }
+            }
+            return result;
         }
     }
 }
